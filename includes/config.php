24428ab5033a5f246bdb21a4097e5833db574f27
“Pritam <prit1605@gmail.com>
2016-03-20 23:59:03 -0300

    Changes the database credentials

--- includes/config.php
+++ includes/config.php
@@ -2,8 +2,8 @@
 
 	// Database constants
 	defined('DB_HOST') ? NULL : define('DB_HOST', 'localhost');
-	defined('DB_USER') ? NULL : define('DB_USER', 'root');
-	defined('DB_PASSWORD') ? NULL : defined('DB_PASSWORD', '');
+	defined('DB_USER') ? NULL : define('DB_USER', 'photo_gallery');
+	defined('DB_PASSWORD') ? NULL : defined('DB_PASSWORD', 'pritam');
 	defined('DB_NAME') ? NULL : defined('DB_NAME', 'photo_gallery');
 
 ?>
