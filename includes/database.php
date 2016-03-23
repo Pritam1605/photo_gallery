bc9f422465232df6cf46fdd4d4d9c51f60ce79cb
“Pritam <prit1605@gmail.com>
2016-03-22 23:48:52 -0300

    Alters the database class to return the class boject instead

--- includes/database.php
+++ includes/database.php
@@ -25,10 +25,6 @@
 			}
 		}
 
-		public function getDbConnection() {
-			return $this->_connection;
-		}
-
 		public function closeDbConnection() {
 			if (isset(self::$_instance)) {
 				mysqli_close($this->_connection);
@@ -42,24 +38,38 @@
 
 		public function query($sql) {
 			$result = mysqli_query($this->_connection, $sql);
-			$this->confirmQuery($result);
+			$this->_confirmQuery($result);
 
 			return $result;
 		}
 
-		private function confirmQuery($result) {
+		private function _confirmQuery($result) {
 			if (!$result) {
-				trigger_error("Connection Error" . mysqli_error(), E_USER_ERROR);
+				trigger_error("Error: " . mysqli_error($this->_connection), E_USER_ERROR);
 			}
 		}
 
-		public function prepSqlString($str) {
+		public function fetchArray($result_set) {
+			return mysqli_fetch_array($result_set);
+		}
+
+		public function escapeValue($str) {
 			return mysqli_real_escape_string($this->_connection, $str);
 		}
 
-	}
+		// returns the number of rows fetched by the query
+		public function numRows($result_set) {
+			return mysqli_num_rows($result_set);
+		}
 
-	$test = MySqlDatabase::getDbInstance();
-	$db = $test->getConnection();
-	pp($db);
+		// returns the number of rows affected by the DML statement
+		public function numAffectedRows() {
+			return mysqli_affected_rows($this->_connection);
+		}
+
+		// returns the last insert id over the connection
+		public function getInsertId() {
+			return mysqli_insert_id($this->_connection);
+		}
+	}
 ?>
---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+	require_once __DIR__ . '/../includes/database.php';
+
+	$db = MySqlDatabase::getDbInstance();
+
+	$sql = <<<SQL
+		Insert INTO user VALUES(10, 'pritam1605', 'pritam', 'pritam', 'bohra');
+SQL;
+
+	$sql = <<<SQL
+		SELECT * FROM user;
+SQL;
+	$result = $db->query($sql);
+	pp($db->fetch_array($result)[1]);
+
+?>
