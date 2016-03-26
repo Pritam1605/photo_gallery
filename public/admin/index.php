6ab9a3ee1b90e1665687edac962d79b76610c79e
Pritam Bohra <prit1605@gmail.com>
2016-03-26 15:02:07 -0300

    Adds the css effect

--- public/admin/index.php
+++ public/admin/index.php
@@ -1,4 +1,28 @@
 <?php
-	session_start();
-	echo 'user id = ' , $_SESSION['user_id'];
+	require_once __DIR__ . '/../../includes/session.php';
+	require_once __DIR__ . '/../../includes/functions.php';
+
+	$session = Session::getSessionInstance();
+	if (!$session->isLoggedIn()) {
+		redirect_to('login.php');
+	}
 ?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Main Menu</title>
+		<link rel="stylesheet" type="text/css" href="../stylesheets/main.css">
+	</head>
+	<body>
+		<div id="header">
+			<h1>Photo Gallery</h1>
+		</div>
+		<div id="main">
+			<h2>Main</h2>
+		</div>
+		<div id="footer">
+			<p> photogallery.com Copyrights <?php echo  date('Y', time()); ?>, Pritam Bohra
+		</div>
+	</body>
+</html>
